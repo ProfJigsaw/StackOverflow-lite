e5e2b9c1c7081888e43ded4036a2ecbb9865232d
ProfJigsaw <dhagodfather@gmail.com>
2018-08-25 15:41:13 +0100

    [finishes #160037590] Integrate CORS into API

--- app.js
+++ app.js
@@ -10,9 +10,17 @@ const port = process.env.PORT || 8080;
 app.engine('dust', cons.dust);
 app.set('view engine', 'dust');
 app.set('views', path.join(__dirname, '/api/v1/views'));
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use(expressApiVersioning({
   apiPath: path.join(__dirname, './api'),
   test: /\/api\/(v[0-9]+).*/,
