6151401331103771264e92aa80c7687dc06870ea
ProfJigsaw <dhagodfather@gmail.com>
2018-08-26 18:48:50 +0100

    [chore #159945148] Changed Database

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('views', path.join(__dirname, '/api/v1/views'));
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
 
