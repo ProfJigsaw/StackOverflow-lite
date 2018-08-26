d0f53ed8a5741e85e8afc8574e4c0b0e2556091a
ProfJigsaw <dhagodfather@gmail.com>
2018-08-26 18:35:13 +0100

    [chore #159945148] Changed Database

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ app.engine('dust', cons.dust);
 app.set('view engine', 'dust');
 app.set('views', path.join(__dirname, '/api/v1/views'));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
