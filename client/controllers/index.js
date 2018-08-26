3fc8e4aabf1ad9a13a21a36baffd13318ce98855
ProfJigsaw <dhagodfather@gmail.com>
2018-08-26 16:37:49 +0100

    [chore #159945148] Changed Database

--- client/controllers/index.js
+++ client/controllers/index.js
@@ -7,7 +7,7 @@ router.get('/', (req, res) => {
   res.send(`
   Welcome to StackOverflow-Lite!
   <p>To see the documetation of this API you can either: </p>
-  <p>Follow <a href="/api-docs">this link<a> or Enter the following in the searchbar: <i>nweze-stackoverflow.herokuapp.com/api-docs.</i> </p>
+  <p>Follow <a href="/api-docs">this link</a> or Enter the following in the searchbar: <i>https://nvc-stackqa.herokuapp.com/api-docs.</i> </p>
   `);
 });
 
